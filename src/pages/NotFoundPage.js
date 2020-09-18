01350d865c192eb4d791ab44b495e0533bda1a32
Matt Hoffner <matthoffner@gmail.com>
2020-09-18 15:52:52 -0700

    fixes

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router-dom';
 
 import Header from '../components/common/Header';
 import Button from '../components/common/Button';
-import NotFoundImage from '../images/Astronaut-big.png';
 import { Image } from '../styles/mixins';
 
 const NotFoundPage = ({ history }) => {
@@ -15,7 +14,6 @@ const NotFoundPage = ({ history }) => {
       </Helmet>
       <Button callback={() => history.push('/')} options={{ left: true }} />
       <Header title={'Not Found - 404'} />
-      <Image src={NotFoundImage} />
     </>
   );
 };
